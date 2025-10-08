f3e4272be1fb29e8a76f4f4107f9f1f7f076286f
Talsview <daviconcyline@gmail.com>
2025-10-07 21:02:50 -0300

    Main.java

--- Animais Domésticos/Main.java
+++ Animais Domésticos/Main.java
@@ -2,19 +2,17 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // Criando lista de animais
         ArrayList<Animal> animais = new ArrayList<>();
-
-        // Adicionando diferentes animais
+        
         animais.add(new Cachorro("Rex", 5));
         animais.add(new Gato("Mimi", 3));
         animais.add(new Papagaio("Loro", 2));
 
-        // Percorrendo a lista e chamando fazerSom()
         for (Animal animal : animais) {
             animal.exibirInformacoes();
             animal.fazerSom();
             System.out.println();
         }
     }
+
 }
